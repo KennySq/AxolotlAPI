a8f92d837223235930800934ba3152da17e1c6af
Kenny91 <odessa5d4@gmail.com>
2021-12-16 14:41:16 +0900

    device memory pool update

---
+++ AxolotlAPI/inc/AXDeviceMemoryPool.cuh
@@ -0,0 +1,41 @@
+#pragma once;
+
+#include"AXUtil.h"
+
+struct AXDeviceMemoryAllocator
+{
+	AXDeviceMemoryAllocator(void* poolPtr, unsigned int size, unsigned int offset);
+	~AXDeviceMemoryAllocator();
+
+	inline void* GetPtr() const
+	{
+		return (void*)((size_t)Raw + Offset);
+	}
+
+	const unsigned int Size;
+	const unsigned int Offset;
+	void* Raw;
+	
+
+};
+
+struct AXDeviceMemoryPool
+{
+public:
+	AXDeviceMemoryPool(unsigned int size);
+	~AXDeviceMemoryPool();
+
+	void Alloc(unsigned int size);
+	void* GetRaw() const
+	{
+		return mRaw;
+	}
+private:
+
+	std::map<void*, std::shared_ptr<AXDeviceMemoryAllocator>> mBlock;
+
+	void* mRaw;
+	unsigned int mOffset;
+
+
+};
---
+++ AxolotlAPI/src/AXDeviceMemoryPool.cu
@@ -0,0 +1,55 @@
+#include<pch.h>
+#include"AXDeviceMemoryPool.cuh"
+
+__device__ void* gPoolRaw = nullptr;
+
+
+AXDeviceMemoryAllocator::AXDeviceMemoryAllocator(void* poolPtr, unsigned int size, unsigned int offset)
+	: Size(size), Raw(poolPtr), Offset(offset)
+{
+	
+}
+
+AXDeviceMemoryAllocator::~AXDeviceMemoryAllocator()
+{
+
+}
+
+AXDeviceMemoryPool::AXDeviceMemoryPool(unsigned int size)
+	: mOffset(0)
+{
+	cudaDeviceProp prop;
+	cudaGetDeviceProperties(&prop, 0);
+
+	std::string memoryStr = std::to_string(prop.totalGlobalMem) + " bytes";
+	std::string sizeStr = std::to_string(size);
+	Log(std::string("Available device memory : ") + memoryStr);
+	Log(std::string("Request ") + sizeStr + " bytes.");
+
+	cudaError_t error = cudaMalloc(&gPoolRaw, size);
+	mRaw = gPoolRaw;
+
+	if (error != NULL)
+	{
+		Log(cudaGetErrorString(error));
+	}
+
+	Log(std::string("Memory requesting success, " + sizeStr + " bytes allocated."));
+}
+
+AXDeviceMemoryPool::~AXDeviceMemoryPool()
+{
+}
+
+void AXDeviceMemoryPool::Alloc(unsigned int size)
+{
+
+	void* ptr = (void*)((size_t)(mRaw) + mOffset);
+
+	std::shared_ptr<AXDeviceMemoryAllocator> ma = std::make_shared<AXDeviceMemoryAllocator>(gPoolRaw, size, mOffset);
+
+	mBlock.insert_or_assign(ptr , ma);
+
+	mOffset += size;
+
+}
