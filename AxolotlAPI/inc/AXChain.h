116a3728a3493e32f4648b4cc39474f873e170a6
Kenny91 <odessa5d4@gmail.com>
2021-12-16 17:46:24 +0900

    AXChain update

--- AxolotlAPI/inc/AXChain.h
+++ AxolotlAPI/inc/AXChain.h
@@ -2,8 +2,17 @@
 #include<Windows.h>
 
 struct AXTexture2D;
+
+struct AX_CHAIN_DESC
+{
+	unsigned int Flag;
+	HWND Hwnd;
+	unsigned int BufferCount;
+};
+
 struct AXChain
 {
+	friend std::shared_ptr<AXChain> AXCreateChain(const AX_CHAIN_DESC& desc);
 public:
 	AXChain();
 	~AXChain();
@@ -13,7 +22,7 @@ public:
 	void GetBuffer(unsigned int index, const std::shared_ptr<AXTexture2D>& outTexture);
 
 private:
-	std::vector<HDC> mMemoryDC;
+	HDC mMemoryDC;
 	std::vector<HBITMAP> mBitmap;
 
 	DWORD* mBuffer;
@@ -21,4 +30,4 @@ private:
 	unsigned int mBufferCount;
 };
 
-std::shared_ptr<AXChain> AXCreateChain(unsigned int flag);
+std::shared_ptr<AXChain> AXCreateChain(unsigned int flag, HWND hWnd);
--- AxolotlAPI/src/AXChain.cpp
+++ AxolotlAPI/src/AXChain.cpp
@@ -14,10 +14,24 @@ void AXChain::Present()
 	
 }
 
-std::shared_ptr<AXChain> AXCreateChain(unsigned int flag)
+std::shared_ptr<AXChain> AXCreateChain(const AX_CHAIN_DESC& desc)
 {
 	std::shared_ptr<AXChain> chain = std::make_shared<AXChain>();
+	unsigned int bufferCount = desc.BufferCount;
+	HDC dc = GetDC(desc.Hwnd);
+	chain->mMemoryDC.push_back(CreateCompatibleDC(dc));
 
+	BITMAPINFO bitmapInfo{};
+
+	bitmapInfo.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+	bitmapInfo.bmiHeader.biBitCount = 
+	
+	for (unsigned int i = 0; i < BufferCount; i++)
+	{
+
+	}
+
+	
 
 
 	return chain;
