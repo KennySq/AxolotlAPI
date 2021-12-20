91f8e66bf118fe9842fe3c70b9dd97c108487168
Ken <odessa5d4@gmail.com>
2021-12-20 23:50:39 +0900

    Debug layer util update

--- AxolotlAPI/inc/AXUtil.h
+++ AxolotlAPI/inc/AXUtil.h
@@ -12,12 +12,27 @@ static void Log(const std::string& str)
 	std::cout <<  std::put_time(&localTime, "%d/%m/%y %H:%M:%S | ") << str << '\n';
 }
 
+#ifdef _DEBUG | DEBUG
+static void DebugLog(const std::string& str)
+{
+	OutputDebugStringA(str.c_str());
+}
+
+static void DebugLog(const char* str)
+{
+	OutputDebugStringA(str);
+}
+#endif
 static void Log(const char* str)
 {
 	auto t = std::time(nullptr);
 	auto localTime = *std::localtime(&t);
 
 	std::cout << std::put_time(&localTime, "%d/%m/%y %H:%M:%S | ") << str << '\n';
+
+#ifdef _DEBUG | DEBUG
+	OutputDebugStringA(str);
+#endif
 }
 
 static void Log(float numeric)
