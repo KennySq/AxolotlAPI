9b9269dfd96f644ff601e12d398cf3f37067b10c
Kenny91 <odessa5d4@gmail.com>
2021-12-30 17:46:41 +0900

    AXBytecode update

--- AxolotlAPI/inc/AXBytecode.h
+++ AxolotlAPI/inc/AXBytecode.h
@@ -2,7 +2,14 @@
 
 struct AXBytecode
 {
+	friend static bool AXCompileFromFile(const char* path, const char* target, const char* entry, unsigned int flag, std::shared_ptr<AXBytecode> pOutBytecode);
 
+	AXBytecode(void* ptr, size_t size);
+	~AXBytecode();
+
+	void* GetPointer() const { return mPtr; }
+	size_t GetSize() const { return mSize; }
 private:
-	void* mPtr;
+	unsigned char* mPtr;
+	size_t mSize;
 };
--- AxolotlAPI/src/AXBytecode.cpp
+++ AxolotlAPI/src/AXBytecode.cpp
@@ -1,2 +1,15 @@
+#include<pch.h>
 #include"AXBytecode.h"
 
+AXBytecode::AXBytecode(void* ptr, size_t size)
+	: mPtr(reinterpret_cast<unsigned char*>(ptr)), mSize(size)
+{
+}
+
+AXBytecode::~AXBytecode()
+{
+	delete[] mPtr;
+	mSize = 0;
+
+	return;
+}
