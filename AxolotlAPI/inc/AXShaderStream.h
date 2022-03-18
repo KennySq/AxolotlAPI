1ef5d21c8486f764189b587e9565911ebf7550c0
KennySq <odessa5d4@gmail.com>
2022-03-18 11:21:51 +0900

    AXShaderStream update

--- AxolotlAPI/inc/AXShaderStream.h
+++ AxolotlAPI/inc/AXShaderStream.h
@@ -5,16 +5,16 @@
 struct AXShaderStream
 {
 public:
-	AXShaderStream();
-	~AXShaderStream();
+	__host__ __device__ AXShaderStream();
+	__host__ __device__ ~AXShaderStream();
 
-	AXFLOAT4* GetInputRegister(size_t index) { return &mInputRegisters.data()[index]; }
-	AXFLOAT4* GetOutputRegister(size_t index) { return &mOutputRegisters.data()[index]; }
-	AXFLOAT4* GetTemporalRegister(size_t index) { return &mTemporalRegisters.data()[index]; }
+	__host__ __device__ AXFLOAT4* GetInputRegister(size_t index) { return &mInputRegisters.data()[index]; }
+	__host__ __device__ AXFLOAT4* GetOutputRegister(size_t index) { return &mOutputRegisters.data()[index]; }
+	__host__ __device__ AXFLOAT4* GetTemporalRegister(size_t index) { return &mTemporalRegisters.data()[index]; }
 
-	void SetInputRegisterValue(size_t index, AXFLOAT4 value) { mInputRegisters[index] = value; }
-	void SetOutputRegisterValue(size_t index, AXFLOAT4 value) { mOutputRegisters[index] = value; }
-	void SetTemporalRegisterValue(size_t index, AXFLOAT4 value) { mTemporalRegisters[index] = value; }
+	__host__ __device__ void SetInputRegisterValue(size_t index, AXFLOAT4 value) { mInputRegisters[index] = value; }
+	__host__ __device__ void SetOutputRegisterValue(size_t index, AXFLOAT4 value) { mOutputRegisters[index] = value; }
+	__host__ __device__ void SetTemporalRegisterValue(size_t index, AXFLOAT4 value) { mTemporalRegisters[index] = value; }
 
 private:
 	std::vector<AXFLOAT4> mInputRegisters;
